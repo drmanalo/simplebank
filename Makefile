179027d68c79575a9469bed3104b2bd8406480b9
drmanalo <drmanalo@outlook.com>
2023-08-23 19:00:12 +0100

    gingonic

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
 include .env
 
+clean:
+	go clean -testcache
+
 migrate-up:
 	migrate -path db/migration -database ${DB_URL} -verbose up
 
 migrate-down:
 	migrate -path db/migration -database ${DB_URL} -verbose down
+
+server:
+	go run main.go
 
 sqlc:
 	sqlc generate
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/drmanalo/simple_bank/api"
+	db "github.com/drmanalo/simple_bank/db/sqlc"
+	"github.com/drmanalo/simple_bank/util"
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/lib/pq"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot load config")
+	}
+
+	if config.Environment == "development" {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
+
+	connPool, err := pgxpool.New(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot connect to db")
+	}
+
+	store := db.NewStore(connPool)
+	server := api.NewServer(store)
+
+	err = server.Start(config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot start server")
+	}
+}
